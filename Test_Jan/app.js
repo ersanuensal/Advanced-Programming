b3848487e9da296e559de4f0648c5f369b503c6c
dr-redtec <jan.leonardi@stud.fra-uas.de>
2021-05-04 20:55:53 +0200

    Test OS Modul
    Memory of the System in JS

--- Test_Jan/app.js
+++ Test_Jan/app.js
@@ -5,8 +5,14 @@ const log = require('./logger')
 // This give information about the path
 const path = require('path');
 
+// This is for the OS information
+const os = require('os');
+
 // This save the path in a var
 var pathObj = path.parse(__filename);
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
 // console.log(logger);
 // This is a private function, app.js dont now about the logger file
@@ -20,3 +26,12 @@ sayHello('Jan');
 
 // Path in the console
 console.log(pathObj);
+
+// console.log('Total Memory: ' + totalMemory);
+
+// Template String
+// ES6 / ES2015 : ECMAScript 6 -> Define what features availble on javascript
+
+// in $ is an argument
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
